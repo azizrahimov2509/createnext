e8cae4fefb1569a0b7c6e354e29b2eead3d21f49
azizrahimov2509 <rahimov2509@mail.com>
2024-07-03 05:08:07 +0300

    moviesite

--- src/app/bookmark/page.tsx
+++ src/app/bookmark/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import searchIcon from "../../../public/search.png";
+import Link from "next/link";
 import MovieItem from "@/components/movieItem";
 import { Movie } from "@/types";
 import SeriesItem from "@/components/seriesItem";
@@ -9,6 +12,7 @@ export default function Page() {
   const [data, setData] = useState<Movie[] | []>([]);
   const [dataSeries, setDataSeries] = useState<Movie[] | []>([]);
   const [refresh, setUpdate] = useState<boolean>(false);
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
   useEffect(() => {
     const value = JSON.parse(localStorage.getItem("favorites") as string) ?? [];
@@ -21,25 +25,40 @@ export default function Page() {
     setDataSeries(value);
   }, [refresh]);
 
+  const filteredMovies = data.filter((item) =>
+    (item.name ?? item.alternativeName)
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase())
+  );
+
+  const filteredSeries = dataSeries.filter((item) =>
+    (item.name ?? item.alternativeName)
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase())
+  );
+
   return (
     <section>
       <div className="container p-5">
         <div className="flex items-center mb-4">
+          <Image src={searchIcon} alt="Search" width={24} height={24} />
           <input
             type="text"
             placeholder="Search for bookmarked shows"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
             className="border rounded-l-lg py-2 px-4 w-[321px] h-[32] outline-none bg-inherit border-none text-xl caret-red-800"
           />
         </div>
 
         {/* Bookmarked Movies */}
         <div className="mt-5 flex flex-col items-start justify-between gap-5">
-          <h2 className="text-3xl from-neutral-400 tracking-[-0.5px] mb-4 border-b-2 border-gray-400 pb-2">
+          <h2 className="text-3xl from-neutral-400 tracking-[-0.5px] mb-4">
             Bookmarked Movies
           </h2>
           <div className="grid grid-cols-1 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 gap-4">
-            {!!data.length ? (
-              data.map((item) => {
+            {!!filteredMovies.length ? (
+              filteredMovies.map((item) => {
                 return (
                   <MovieItem
                     data={{ ...item, setUpdate: setUpdate }}
@@ -57,12 +76,12 @@ export default function Page() {
 
         {/* Bookmarked TV Series */}
         <div className="mt-5 flex flex-col items-start justify-between gap-5">
-          <h2 className="text-3xl from-neutral-400 tracking-[-0.5px] mb-4 mt-4 border-b-2 border-gray-400 pb-2">
+          <h2 className="text-3xl from-neutral-400 tracking-[-0.5px] mb-4 mt-4">
             Bookmarked TV Series
           </h2>
           <div className="grid grid-cols-1 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 gap-4">
-            {!!dataSeries.length ? (
-              dataSeries.map((item) => {
+            {!!filteredSeries.length ? (
+              filteredSeries.map((item) => {
                 return (
                   <SeriesItem
                     data={{ ...item, setUpdate: setUpdate }}
