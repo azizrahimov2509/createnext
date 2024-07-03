34a72c49161ed756eb9a7b488c78afce38c06067
azizrahimov2509 <rahimov2509@mail.com>
2024-07-03 04:01:34 +0300

    moviesite

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -2,21 +2,29 @@
 import React from "react";
 import Image from "next/image";
 import searchIcon from "../../../public/search.png";
-
 import Link from "next/link";
 import { getContent } from "../queries";
 import MovieItem from "@/components/movieItem";
-import { log } from "console";
 import { Movie } from "@/types";
 
 export default async function page() {
-  const request = await getContent("movie");
+  let movies: Movie[] = [];
 
-  log(request.docs[0]);
+  try {
+    const request = await getContent("movie");
+    // Проверяем, что request и request.docs существуют и являются массивом
+    if (request && request.docs && Array.isArray(request.docs)) {
+      movies = request.docs;
+    } else {
+      console.error("Invalid response format from getContent.");
+    }
+  } catch (error) {
+    console.error("Error fetching content:", error);
+  }
 
   return (
     <section>
-      <div className="container  p-4">
+      <div className="container p-4">
         <div className="flex items-center mb-4">
           <Image src={searchIcon} alt="Search" width={24} height={24} />
           <input
@@ -26,16 +34,19 @@ export default async function page() {
           />
         </div>
 
-        {""}
         <div className="mt-5 flex flex-col items-start justify-between gap-4">
           <h2 className="text-3xl from-neutral-400 tracking-[-0.5px] mb-4 border-b-2 border-gray-400 pb-2">
             Movies
           </h2>
 
           <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-            {request?.docs?.map((movie: Movie) => (
-              <MovieItem data={movie} key={movie.id} />
-            ))}
+            {movies.length > 0 ? (
+              movies.map((movie: Movie) => (
+                <MovieItem data={movie} key={movie.id} />
+              ))
+            ) : (
+              <p>No movies available.</p>
+            )}
           </div>
         </div>
       </div>
