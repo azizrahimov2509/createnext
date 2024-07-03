d65d3d4d21485a6dad73b0855b4273d8af98f1a1
azizrahimov2509 <rahimov2509@mail.com>
2024-07-03 04:28:02 +0300

    movieSite

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -33,7 +33,7 @@ export default async function page() {
           </h2>
 
           <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-            {request?.docs?.map((movie: Movie) => (
+            {request.docs?.map((movie: Movie) => (
               <MovieItem data={movie} key={movie.id} />
             ))}
           </div>
