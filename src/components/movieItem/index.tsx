ef48807f1db2d6a5ab012b694bbbe41c40d510bd
azizrahimov2509 <rahimov2509@mail.com>
2024-07-03 04:42:20 +0300

    moviesite

--- src/components/movieItem/index.tsx
+++ src/components/movieItem/index.tsx
@@ -29,7 +29,7 @@ export default function MovieItem({ data }: MovieItemProps) {
     type,
     alternativeName,
     countries,
-    genres,
+    genres = [], // Добавлено значение по умолчанию для genres
     rating,
   } = data;
 
@@ -152,12 +152,16 @@ export default function MovieItem({ data }: MovieItemProps) {
           </h3>
           <h3 className="pb-1">
             • <span className="from-neutral-800">Genre: </span>
-            {genres?.map((genre, index) => (
-              <span key={index}>
-                {genre.name}
-                {index < genres.length - 1 ? ", " : ""}
-              </span>
-            ))}
+            {genres.length > 0 ? (
+              genres.map((genre, index) => (
+                <span key={index}>
+                  {genre.name}
+                  {index < genres.length - 1 ? ", " : ""}
+                </span>
+              ))
+            ) : (
+              <span>N/A</span>
+            )}
           </h3>
 
           <h3 className="pb-4">
